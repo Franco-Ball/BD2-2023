a307e272a71b5a0ea5991949b2324f63fe814cda
Franco-Ball <francoball181@gmail.com>
2023-06-13 18:55:47 -0300

    added clase 11

---
+++ Class11.sql
@@ -0,0 +1,29 @@
+-- Active: 1652961517460@@127.0.0.1@3306@sakila
+use sakila;
+
+#Class11
+#Find all the film titles that are not in the inventory.
+
+SELECT * FROM
+
+#Find all the films that are in the inventory but were never rented.
+
+
+
+#Show title and inventory_id.
+#This exercise is complicated.
+#hint: use sub-queries in FROM and in WHERE or use left join and ask if one of the fields is null
+#Generate a report with:
+
+
+
+#customer (first, last) name, store id, film title,
+#when the film was rented and returned for each of these customers
+#order by store_id, customer last_name
+#Show sales per store (money of rented films)
+
+
+
+#show store's city, country, manager info and total sales (money)
+#(optional) Use concat to show city and country and manager first and last name
+#Which actor has appeared in the most films?
