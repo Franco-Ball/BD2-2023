7423063e7462b1704835163430cee47f88df2cb9
Franco Ball <francoball181@gmail.com>
2023-09-05 21:09:11 -0300

    Class19

--- Class19.sql
+++ Class19.sql
@@ -8,6 +8,29 @@ IDENTIFIED BY "pepe1234";
 GRANT SELECT, UPDATE, DELETE ON sakila.* TO 'data_analyst'@'localhost';
 
 #3
-create table test (id int, nombre varchar(30));
+create table test (id int primary key, nombre varchar(30));
+
+/*
+ ERROR 1142 (42000): CREATE command denied to user 'data_analyst'@'localhost' for table 'test'
+ Este error ocurre porque estamos intentando crear una tabla cuando el usuario data_analyst no tiene los permisos necesarios para hacerlo, 
+ ya que posteriormente solo le asignamos los permisos SELECT, UPDATE y CREATE
+ */
 
 #4
+
+UPDATE film SET title = "LA PALERMONETA" WHERE film_id = 500;
+
+/*Anda correctamente porque tiene los permisos*/
+
+#5
+
+REVOKE UPDATE ON sakila.* FROM data_analyst;
+
+#6
+
+UPDATE film SET title = "EL GALLINERO" WHERE film_id = 500;
+
+/*
+ ERROR 1142 (42000): UPDATE command denied to user 'data_analyst'@'localhost' for table 'film'
+ Este error ocurre porque le acabamos de sacar los permisos para editar tablas en la actividad numero 5
+ */
